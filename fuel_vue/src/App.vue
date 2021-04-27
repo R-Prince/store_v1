b43c58d06770003c46f517ee81741017875c0d9f
R-Prince <reneprince93@icloud.com>
2021-04-27 22:12:35 +0000

    Add navbar to homepage

--- fuel_vue/src/App.vue
+++ fuel_vue/src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="nav">
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item"><strong>Fuel</strong></router-link>
+            <a class="navbar-burger" aria-label="menu" aria-expanded="false"    data-target="navbar-menu">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div class="navbar-menu" id="navbar-menu">
+            <div class="navbar-end">
+                <router-link to="/protein" class="navbar-item">Protein</router-link>
+                <router-link to="/shakers" class="navbar-item">Shakers</router-link>
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/log-in" class="button is-light">Log in</router-link>
+                        <router-link to="/cart" class="button is-success">
+                            <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                            <span>Cart</span>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <section class="section">
+        <router-view/>
+  </section>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
